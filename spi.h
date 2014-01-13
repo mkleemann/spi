1dde543ccb3635c9535fa30f9986aa09503800e4
Matthias Kleemann <dev@layer128.net>
2014-01-13 21:46:37 +0100

    cleanup includes and update README
    - remove ../ from include paths
    - update README in regards of cmake

--- spi.h
+++ spi.h
@@ -22,7 +22,7 @@
 #ifndef SPI_H_
 #define SPI_H_
 
-#include "../config/spi_config.h"
+#include "config/spi_config.h"
 
 /***************************************************************************/
 /* CALCULATE BIT VALUES                                                    */
